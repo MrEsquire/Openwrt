0bd34c95e8a9a1098eb2e07683a22e99f36f2db9
MrEsquire <dr.soulsolo@hotmail.com>
2021-03-09 16:04:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,6 @@
 cd package/lean
 rm -rf luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+cd feeds/luci/applications/
+wget https://github.com/pymumu/luci-app-smartdns/archive/lede.zip && unzip lede.zip
+rm lede.zip
